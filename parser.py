f6b465e7e4bee11cf872bbb5c23c1c42940821e7
John Lin <jlin22@stuy.edu>
2018-03-04 20:54:14 -0500

    testing

--- parser.py
+++ parser.py
@@ -1,42 +1,14 @@
-
 from display import *
 from matrix import *
 from draw import *
 
-"""
-Goes through the file named filename and performs all of the actions listed in that file.
-The file follows the following format:
-     Every command is a single character that takes up a line
-     Any command that requires arguments must have those arguments in the second line.
-     The commands are as follows:
-         line: add a line to the edge matrix - 
-	    takes 6 arguemnts (x0, y0, z0, x1, y1, z1)
-	 ident: set the transform matrix to the identity matrix - 
-	 scale: create a scale matrix, 
-	    then multiply the transform matrix by the scale matrix - 
-	    takes 3 arguments (sx, sy, sz)
-	 move: create a translation matrix, 
-	    then multiply the transform matrix by the translation matrix - 
-	    takes 3 arguments (tx, ty, tz)
-	 rotate: create a rotation matrix,
-	    then multiply the transform matrix by the rotation matrix -
-	    takes 2 arguments (axis, theta) axis should be x, y or z
-	 apply: apply the current transformation matrix to the 
-	    edge matrix
-	 display: draw the lines of the edge matrix to the screen
-	    display the screen
-	 save: draw the lines of the edge matrix to the screen
-	    save the screen to a file -
-	    takes 1 argument (file name)
-	 quit: end parsing
-See the file script for an example of the file format
-"""
 def parse_file( fname, points, transform, screen, color ):
 	i = 0
 	funct = ""
 	with open(fname) as file_object:
 		for line in file_object:
 			if i==0:
+				i = (i+1)%2
 				#function names
 				if line[:4] == "line":
 					funct = "line"
@@ -54,29 +26,61 @@ def parse_file( fname, points, transform, screen, color ):
 					funct = 'display'
 				elif line[:4] == 'save':
 					funct = 'save'
+				elif line[:4] == 'quit':
+					funct = 'quit'
 			elif i == 1:
 				if funct == "line":
 					a = parse_args(6, line)
 					add_edge(points, a[0], a[1], a[2], a[3], a[4], a[5])
+					i = (i + 1) % 2
 				elif funct == "ident":
-					funct = 'ident'
+					ident(transform)
 				elif funct == 'scale':
-					funct = 'scale'
+					a = parse_args(3, line)
+					matrix_mult(make_scale(a[0], a[1], a[2]), transform)
+					i = (i + 1) % 2
+
 				elif funct == 'move':
-					funct = 'move'
+					a = parse_args(3, line)
+					matrix_mult(make_translate(a[0], a[1], a[2]), transform)
+					i = (i + 1) % 2
 				elif funct == 'rotate':
-					funct = 'rotate'
+					i = (i + 1) % 2
+					a = parse_args(2, line)
+					if a[0] == 'x':
+						matrix_mult(make_rotX(a[1]), transform)
+					elif a[0] == 'y':
+						matrix_mult(make_rotY(a[1]), transform)
+					elif a[0] == 'z':
+						matrix_mult(make_rotZ(a[1]), transform)
+
 				elif funct == 'apply':
-					funct = 'apply'
+					matrix_mult(transform, points)
 				elif funct == 'display':
-					funct = 'display'
+					draw_lines(points, screen, color)
+					display(screen)
 				elif funct == 'save':
-					funct = 'save'
-			
-	return 0
+					a = parse_args(1, line)
+					draw_lines(points, screen, color)
+					save_extension(screen, "img.jpg")
+				elif funct == 'quit':
+					return
+	pass
+
 def parse_args(index, string):
 	list = []
 	for i in range(index):
-		list.append(string[:indexOf(" ")])
-		string = string[indexOf(" ")+1:]
+		if i != index - 1:
+			list.append(string[:string.index(" ")])
+			string = string[string.index(" ")+1:]
+		else:
+			list.append(string[:string.index("\n")])
 	return list
+
+
+screen = new_screen()
+color = [ 0, 255, 0 ]
+edges = []
+transform = new_matrix()
+parse_file( 'script', edges, transform, screen, color )
+print_matrix(edges)
